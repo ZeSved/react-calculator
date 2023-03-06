a0690d33b42c27ffa5f8b7966d4b4c7e177f608c
Hexy32 <sunshine.goody8762@gmail.com>
2023-03-06 11:32:07 -0500

    Fixed the arithmetic function

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,106 @@
-import { MouseEvent, useState } from 'react'
-import styles from './App.module.css'
 import Buttons, { Button } from './components/Buttons'
-import { arithmeticFunc } from './utils/systemwork'
+import { MouseEvent, useState } from 'react'
+
+import { arithmeticFunc } from './utils/helpers'
+import styles from './App.module.css'
+
+const ARITHMETIC_ARR = ['/', '-', '*', '+']
 
 function App() {
-	const [input, setInput] = useState<string | null>('')
-	const arithmeticArr = ['/', '-', '*', '+']
+  const [input, setInput] = useState<string | null>('')
 
-	const buttons: Button[] = [
-		{
-			content: '1',
-		},
-		{
-			content: '2',
-		},
-		{
-			content: '3',
-		},
-		{
-			content: '*',
-		},
-		{
-			content: '4',
-		},
-		{
-			content: '5',
-		},
-		{
-			content: '6',
-		},
-		{
-			content: '/',
-		},
-		{
-			content: '7',
-		},
-		{
-			content: '8',
-		},
-		{
-			content: '9',
-		},
-		{
-			content: '+',
-		},
-		{
-			content: 'CE',
-			func: clear,
-		},
-		{
-			content: '0',
-		},
-		{
-			content: '=',
-			func: equal,
-		},
-		{
-			content: '-',
-		},
-	]
+  const buttons: Button[] = [
+    {
+      content: '1',
+    },
+    {
+      content: '2',
+    },
+    {
+      content: '3',
+    },
+    {
+      content: '*',
+    },
+    {
+      content: '4',
+    },
+    {
+      content: '5',
+    },
+    {
+      content: '6',
+    },
+    {
+      content: '/',
+    },
+    {
+      content: '7',
+    },
+    {
+      content: '8',
+    },
+    {
+      content: '9',
+    },
+    {
+      content: '+',
+    },
+    {
+      content: 'CE',
+      func: clear,
+    },
+    {
+      content: '0',
+    },
+    {
+      content: '=',
+      func: equal,
+    },
+    {
+      content: '-',
+    },
+  ]
 
-	function normalOperation(event: MouseEvent) {
-		const elem = event.target as HTMLButtonElement
-		const lastPressedButton = elem.textContent
+  function normalOperation(event: MouseEvent) {
+    const elem = event.target as HTMLButtonElement
+    const lastPressedButton = elem.textContent!
 
-		for (let i = 0; i >= arithmeticArr.length; i++) {
-			if (lastPressedButton == arithmeticArr[i]) {
-				arithmeticFunc(arithmeticArr, lastPressedButton, setInput, input)
-				return
-			}
-		}
+    if (input === null) return setInput(lastPressedButton)
 
-		if (input === null) setInput(lastPressedButton)
-		else setInput(input! + lastPressedButton)
-	}
+    if (ARITHMETIC_ARR.includes(lastPressedButton)) {
+      if (arithmeticFunc(ARITHMETIC_ARR, input)) return
+    }
 
-	function equal() {
-		setInput(Function('return ' + input)())
-	}
+    setInput(input! + lastPressedButton)
+  }
 
-	function clear() {
-		setInput(null)
-	}
+  function equal() {
+    setInput(solveExpression())
 
-	return (
-		<>
-			<main className={styles.wrapper}>
-				<div className={styles.calculatorContainer}>
-					<input
-						value={input ? input : '0'}
-						type='text'
-						disabled
-						className={styles.output}
-					/>
-					<Buttons
-						defaultFunction={normalOperation}
-						buttons={buttons}
-					/>
-				</div>
-			</main>
-		</>
-	)
+    function solveExpression() {
+      try {
+        return Function('return ' + input)()
+      } catch (error) {
+        return error
+      }
+    }
+  }
+
+  function clear() {
+    setInput(null)
+  }
+
+  return (
+    <>
+      <main className={styles.wrapper}>
+        <div className={styles.calculatorContainer}>
+          <input value={input ? input : '0'} type="text" disabled className={styles.output} />
+          <Buttons defaultFunction={normalOperation} buttons={buttons} />
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default App
--- src/utils/systemwork.ts
+++
@@ -1,5 +0,0 @@
-export function arithmeticFunc(arithArr: string[], pressedButton: string, inputSet: any, input: any) {
-  for (let i = 0; i >= arithArr.length; i++) {
-    if (input.endswith(arithArr[i])) return
-  }
-}
